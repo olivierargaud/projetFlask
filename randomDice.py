2e1865640ab737b4f09044c2e48c565cc274a3aa
olivierargaud <olivier.argaud@ipilyon.net>
2020-10-19 17:16:39 +0200

    fichier home renommé login ajout nouveauCompte et modification gitignore

--- randomDice.py
+++ randomDice.py
@@ -1,6 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-     return render_template('home.html')
+def loginDeBase():
+     return render_template('login.html')
+
+@app.route('/',methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        return render_template('login.html')  
+    else:
+        return redirect('/')
+
+@app.route('/nouveauCompte')
+def nouveauCompteDeBase():
+    return render_template('nouveauCompte.html')
+
+@app.route('/nouveauCompte',methods=['POST','GET'])
+def nouveauCompte():
+    if request.method == 'POST':
+        return render_template('nouveauCompte.html')  
+    else:
+        return redirect('/')
