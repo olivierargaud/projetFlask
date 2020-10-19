473ca6a4143e963fb6a27c07cdc102c0e974d005
olivierargaud <olivier.argaud@ipilyon.net>
2020-10-19 17:40:11 +0200

    ajout de la base sql et des differentes tables

--- randomDice.py
+++ randomDice.py
@@ -1,5 +1,59 @@
 from flask import Flask,render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///baseRandomDice.db'
+db = SQLAlchemy(app)
+
+
+class dice(db.Model):
+    """ table qui definit les dés """
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    value = db.Column(db.Integer, nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Dé %r %r>' % (self.name , self.value)
+
+
+class dice_group(db.Model):
+    """ table qui definit les groupe de dés (lancer plusieurs dés) """
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    children = db.relationship("dice_list_group")
+
+    def __repr__(self):
+        return '<groupe %r>' % (self.name)
+
+
+class dice_list_group(db.Model):
+    """ table de liaison entre les dés et les groupes de dés """
+    id = db.Column(db.Integer, primary_key = True)
+    idDice = db.Column(db.Integer)
+    idGroup = db.Column(db.Integer, ForeignKey('dice_group.id'))
+    
+    def __repr__(self):
+        return '<idDice %r %r>' % (self.idDice , self.idGroup)
+
+
+
+
+class user(db.Model):
+    """ table qui definit les utilisateurs """
+    login = db.Column(db.String(200), nullable = False , primary_key = True)
+    mdp = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+
+    def __repr__(self):
+        return '<User %r >' % self.name 
+
 
 @app.route('/')
 def loginDeBase():
